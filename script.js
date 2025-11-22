de0843246077dd398cc4d216d06a6ec41e040609
vanndebush <bevannstivens7@gmail.com>
2025-11-22 14:36:33 +0700

    Fix array side-effects and improve input validation

--- script.js
+++ script.js
@@ -6,20 +6,16 @@ function sum(inputArray) {
   return total;
 }
 function average(inputArray) {
-  var total = 0;
-  for (var i = 0; i < inputArray.length; i++) {
-    total += inputArray[i];
-  }
-  return total / inputArray.length;
+  return sum(inputArray) / inputArray.length;
 }
 function max(inputArray) {
-  var sortedArray = inputArray.sort(function(a, b) {
+  var sortedArray = inputArray.slice().sort(function(a, b) {
     return b - a;
   });
   return sortedArray[0];
 }
 function min(inputArray) {
-  var sortedArray = inputArray.sort(function(a, b) {
+  var sortedArray = inputArray.slice().sort(function(a, b) {
     return a - b;
   });
   return sortedArray[0];
@@ -29,12 +25,22 @@ var repeat = true;
 var inputArray = [];
 
 while (repeat) {
-  var input = parseInt(prompt('Input a number!\n\n(Click "Cancel" to show the result)'));
-  if (input || input === 0) {
-    inputArray.push(input);
-  } else {
-    alert('[ ' + inputArray.join(', ') + ' ]\n\nSum: ' + sum(inputArray) + '\nAverage: ' + average(inputArray) + '\nMax: ' + max(inputArray) + '\nMin: ' + min(inputArray));
+  var inputString = prompt('Input a number!\n\n(Click "Cancel" to show the result)');
+
+  if (inputString === null || inputString === '') {
+    if (inputArray.length === 0) {
+      alert('No numbers entered!');
+    } else {
+      alert('[ ' + inputArray.join(', ') + ' ]\n\nSum: ' + sum(inputArray) + '\nAverage: ' + average(inputArray) + '\nMax: ' + max(inputArray) + '\nMin: ' + min(inputArray));
+    }
     repeat = false;
+  } else {
+    var inputNumber = parseInt(inputString);
+    if (isNaN(inputNumber)) {
+      alert('Invalid input! Please enter a number.');
+    } else {
+      inputArray.push(inputNumber);
+    }
   }
 }
 
